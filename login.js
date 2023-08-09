b9ce71523ecc3d48ab86742b60ccb077131edd2d
CslTest1 <141842517+CslTest1@users.noreply.github.com>
2023-08-09 10:57:06 -0300

    Create login.js

---
+++ login.js
@@ -0,0 +1,36 @@
+// Função para fazer o login com Bearer Token
+function fazerLoginComToken() {
+  // Bearer Token do usuário
+  const token = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.RxvAfAaGK5REuXWL-4VbQZYgQgRDCkZk0i5C9U5h3KI
+';
+
+  // URL de login
+  const url = 'https://ormuspay.com/login';
+
+  // Configuração da requisição POST
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+      'Authorization': `Bearer ${token}`
+    }
+  };
+
+  // Faz a requisição POST para a URL
+  fetch(url, requestOptions)
+    .then(response => response.json())
+    .then(data => {
+      if (data.status === 'success') {
+        // Login bem-sucedido, realizar ações pós-login aqui
+        console.log('Login realizado com sucesso:', data.message);
+      } else {
+        // Login falhou, lidar com o erro aqui
+        console.error('Erro no login:', data.error);
+      }
+    })
+    .catch(error => {
+      console.error('Erro na requisição:', error);
+    });
+}
+
+const botaoLogin = document.getElementById('botao-login');
+botaoLogin.addEventListener('click', fazerLoginComToken);
